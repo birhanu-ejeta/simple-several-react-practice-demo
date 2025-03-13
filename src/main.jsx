8f935ed089345f6e32da276564ddcfe1f5bdc08c
birhanu-ejeta <birhanuejeta65@gmail.com>
2025-03-13 21:06:26 +0300

    load more data with api

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from './App.jsx'
 import RandomColor from './components/random-color/Index'
 import StarRating from './components/Star-rating'
 import ImageSlider from './components/image-slider'
+import LoadMoreData from './components/load-more-data'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
@@ -14,6 +15,9 @@ createRoot(document.getElementById('root')).render(
     {/* star rating  */}
     {/* <StarRating noOfStars={10}/> */}
     {/* image slider container  */}
-    <ImageSlider url={"https://picsum.photos/v2/list"} limit={10}/>
+    {/* <ImageSlider url={"https://picsum.photos/v2/list"} limit={10}/> */}
+
+    {/* load multi data  */}
+    <LoadMoreData/>
   </StrictMode>,
 )
